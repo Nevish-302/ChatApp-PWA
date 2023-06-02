f885a438749fc987a34769094cb9750e9e9944c8
Nevish-302 <shiven302@gmail.com>
2023-06-02 20:38:11 +0530

    added-icon-11

--- manifest.js
+++ manifest.js
@@ -1,13 +1,13 @@
 const Manifest = {
     registerType: "prompt",
-    includeAssets: ["./src/assets/images/attach.png", "./src/assets/images/back.png", "./src/assets/images/document.png", "./src/assets/images/edit.png", "./src/assets/images/members.png", "./src/assets/images/Meme.png", "./src/assets/images/options.png", "./src/assets/images/phone.png", "./src/assets/images/photo.png", "./src/assets/images/report.png", "./src/assets/images/send.png", "./src/assets/images/video.png", "./public/icon.png"],
+    includeAssets: ["./src/assets/images/attach.png", "./src/assets/images/back.png", "./src/assets/images/document.png", "./src/assets/images/edit.png", "./src/assets/images/members.png", "./src/assets/images/Meme.png", "./src/assets/images/options.png", "./src/assets/images/phone.png", "./src/assets/images/photo.png", "./src/assets/images/report.png", "./src/assets/images/send.png", "./src/assets/images/video.png", "./src/assets/react.svg", "./public/icon.png"],
     manifest: {
     name: "Chat App PWA",
     short_name:"ChatApp",
     description: "An app that contains chats for the trip planned.",
     icons: [
     {
-    src: "./public/icon.png",
+    src: "./src/assets/react.svg",
     sizes: "512x512",
     type: "image/png",
     purpose: "any maskable"
