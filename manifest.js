9a536d4ecde161a77c168af676109546c65c8f6c
Nevish-302 <shiven302@gmail.com>
2023-06-02 20:31:03 +0530

    added-icon-8

--- manifest.js
+++ manifest.js
@@ -1,6 +1,6 @@
 const Manifest = {
     registerType: "prompt",
-    includeAssets: ["./src/assets/images/attach.png", "./src/assets/images/back.png", "./src/assets/images/document.png", "./src/assets/images/edit.png", "./src/assets/images/members.png", "./src/assets/images/Meme.png", "./src/assets/images/options.png", "./src/assets/images/phone.png", "./src/assets/images/photo.png", "./src/assets/images/report.png", "./src/assets/images/send.png", "./src/assets/images/video.png", "./icon.png"],
+    includeAssets: ["./src/assets/images/attach.png", "./src/assets/images/back.png", "./src/assets/images/document.png", "./src/assets/images/edit.png", "./src/assets/images/members.png", "./src/assets/images/Meme.png", "./src/assets/images/options.png", "./src/assets/images/phone.png", "./src/assets/images/photo.png", "./src/assets/images/report.png", "./src/assets/images/send.png", "./src/assets/images/video.png", "./icon.png", "./public/icon.png"],
     manifest: {
     name: "Chat App PWA",
     short_name:"ChatApp",
